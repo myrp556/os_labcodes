e297f2c8dcf0f10b6d216ea8f9cfb81a1cf7b0f9
os_lab_user <you@example.com>
2015-05-07 23:13:10 +0800

    lab6

--- lab6/kern/trap/trap.c
+++ lab6/kern/trap/trap.c
@@ -42,7 +42,7 @@ static struct pseudodesc idt_pd = {
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
-     /* LAB1 YOUR CODE : STEP 2 */
+     /* LAB1 2012011288 : STEP 2 */
      /* (1) Where are the entry addrs of each Interrupt Service Routine (ISR)?
       *     All ISR's entry addrs are stored in __vectors. where is uintptr_t __vectors[] ?
       *     __vectors[] is in kern/trap/vector.S which is produced by tools/vector.c
@@ -54,16 +54,22 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
-     /* LAB5 YOUR CODE */ 
+     /* LAB5 2012011288 */ 
      //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
      //so you should setup the syscall interrupt gate in here
-    extern uintptr_t __vectors[];
-    int i;
-    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
-        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
-    }
-    SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
-    lidt(&idt_pd);
+      extern uintptr_t __vectors[];//声明外部变量
+      //所有的中断服务程序都存储在vectors中
+      //__vectors[]出现在kern/trap/vector.S中
+      //在执行了make之后就可以基于tools/vector.c生成vector.S
+      int i;
+      for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++)
+      	SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+      
+      SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
+      //启用中断描述符表中的每一个条目，以便于从用户态转到内核态
+      
+      lidt(&idt_pd);
+      //使用lidt指令告诉CPU中断描述符表的位置
 }
 
 static const char *
@@ -218,22 +224,30 @@ trap_dispatch(struct trapframe *tf) {
         break;
     case IRQ_OFFSET + IRQ_TIMER:
 #if 0
-    LAB3 : If some page replacement algorithm(such as CLOCK PRA) need tick to change the priority of pages, 
+    LAB3 : If some page replacement algorithm(such as CLOCK PRA) need tick to change the priority of pages,
     then you can add code here. 
 #endif
-        /* LAB1 YOUR CODE : STEP 3 */
+        /* LAB1 2012011288 : STEP 3 */
         /* handle the timer interrupt */
         /* (1) After a timer interrupt, you should record this event using a global variable (increase it), such as ticks in kern/driver/clock.c
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
-        /* LAB5 YOUR CODE */
+        /* LAB5 2012011288*/
         /* you should upate you lab1 code (just add ONE or TWO lines of code):
          *    Every TICK_NUM cycle, you should set current process's current->need_resched = 1
          */
-        ticks ++;
-        assert(current != NULL);
-        run_timer_list();
+        /* LAB6 YOUR CODE */
+        /* IMPORTANT FUNCTIONS:
+	     * run_timer_list
+	     *----------------------
+	     * you should update your lab5 code (just add ONE or TWO lines of code):
+         *    Every tick, you should update the system time, iterate the timers, and trigger the timers which are end to call scheduler.
+         *    You can use one funcitons to finish all these things.
+         */
+		ticks++;
+		assert(current != NULL);
+		run_timer_list();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
@@ -296,4 +310,5 @@ trap(struct trapframe *tf) {
         }
     }
 }
+
 
